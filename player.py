179cbc98af0acb5977374c29ff0806aff5cb47ec
N8Brown <nathan.a.brown@outlook.com>
2021-05-12 15:25:47 -0400

    Update code to include course section 31

--- player.py
+++ player.py
@@ -4,16 +4,23 @@ from mutagen.mp3 import MP3
 import pygame
 import os
 import time
+import tkinter.ttk as ttk
 
+
+# CREATE GUI WINDOW
 root = Tk()
-
 root.title("MP3 Player")
 root.geometry("500x400")
+
+
+# INITIALIZE PYGAME FOR AUDIO
+pygame.mixer.init()
 
 
 # GLOBAL VARIABLES
 is_paused = False
 playlist = []
+
 
 # FUNCTIONS
 def add_song():
@@ -118,58 +125,22 @@ def play_time():
     raw_song_length = song_mut.info.length
     converted_song_length = time.strftime('%M:%S', time.gmtime(raw_song_length))
 
-    test_label.config(text=converted_song_length)
-
     if current_position >= 1:
         status_bar.config(text=f'Time Elapsed: {current_time} of {converted_song_length}  ')
     
     status_bar.after(1000, play_time)
 
 
-
-# Initialize PyGame
-
-pygame.mixer.init()
-
-# Create playlist box
-playlist_box = Listbox(root, bg="black", fg="green", width=60, selectbackground="green", selectforeground="black")
-playlist_box.pack(pady=20)
+def volume(x):
+    pygame.mixer.music.set_volume(volume_slider.get())
+    test_label.config(text=volume_slider.get())
 
 
-
-# Create media control buttons
-# Control frame to group the buttons in
-control_frame = Frame(root)
-control_frame.pack(pady=20)
-
-# Define button images
-back_btn_img = PhotoImage(file="images/back50.png")
-forward_btn_img = PhotoImage(file="images/forward50.png")
-play_btn_img = PhotoImage(file="images/play50.png")
-pause_btn_img = PhotoImage(file="images/pause50.png")
-stop_btn_img = PhotoImage(file="images/stop50.png")
-
-# Create the actual buttons and link them to the control frame
-back_btn = Button(control_frame, image=back_btn_img, borderwidth=0, command=previous_song)
-forward_btn = Button(control_frame, image=forward_btn_img, borderwidth=0, command=next_song)
-play_btn = Button(control_frame, image=play_btn_img, borderwidth=0, command=play_song)
-pause_btn = Button(control_frame, image=pause_btn_img, borderwidth=0, command=pause_song)
-stop_btn = Button(control_frame, image=stop_btn_img, borderwidth=0, command=stop_song)
-
-# Place the buttons on the screen
-back_btn.grid(row=0, column=0, padx=10)
-forward_btn.grid(row=0, column=1, padx=10)
-play_btn.grid(row=0, column=2, padx=10)
-pause_btn.grid(row=0, column=3, padx=10)
-stop_btn.grid(row=0, column=4, padx=10)
-
-
-# Create menu
-
+# GUI WINDOW MENU
 main_menu = Menu(root)
 root.config(menu=main_menu)
 
-# Create Add Song sub-menu
+# Create Playlist menu
 playlist_menu = Menu(main_menu, tearoff=0)
 main_menu.add_cascade(label="Playlist", menu=playlist_menu)
 
@@ -179,7 +150,51 @@ playlist_menu.add_separator()
 playlist_menu.add_command(label="Delete Song", command=delete_song)
 playlist_menu.add_command(label="Clear Playlist", command=delete_all_songs)
 
-# Create status bar
+
+# GUI WIDGETS AND FEATURES
+
+# Frame to hold Playlist, Volume Control, Audio Controls 
+main_frame = Frame(root)
+main_frame.pack(pady=20)
+
+# Playlist box
+playlist_box = Listbox(main_frame, bg="black", fg="green", width=60, selectbackground="green", selectforeground="black")
+playlist_box.grid(row=0, column=0)
+
+# Volume frame to hold Volume Slider
+volume_frame = LabelFrame(main_frame, text="Volume")
+volume_frame.grid(row=0, column=1, padx=20)
+
+# Volume slider
+volume_slider = ttk.Scale(volume_frame, from_=1, to=0, value=.5, orient=VERTICAL, length=125, command=volume) #add 'value' attribute and to set a default starting value (between from_ and to values)
+volume_slider.pack(pady=10)
+
+# Control frame to hold Audio Control buttons
+control_frame = Frame(main_frame)
+control_frame.grid(row=1, column=0, pady=20)
+
+# Audio Control buttons
+back_btn_img = PhotoImage(file="images/back50.png")
+back_btn = Button(control_frame, image=back_btn_img, borderwidth=0, command=previous_song)
+back_btn.grid(row=0, column=0, padx=10)
+
+forward_btn_img = PhotoImage(file="images/forward50.png")
+forward_btn = Button(control_frame, image=forward_btn_img, borderwidth=0, command=next_song)
+forward_btn.grid(row=0, column=1, padx=10)
+
+play_btn_img = PhotoImage(file="images/play50.png")
+play_btn = Button(control_frame, image=play_btn_img, borderwidth=0, command=play_song)
+play_btn.grid(row=0, column=2, padx=10)
+
+pause_btn_img = PhotoImage(file="images/pause50.png")
+pause_btn = Button(control_frame, image=pause_btn_img, borderwidth=0, command=pause_song)
+pause_btn.grid(row=0, column=3, padx=10)
+
+stop_btn_img = PhotoImage(file="images/stop50.png")
+stop_btn = Button(control_frame, image=stop_btn_img, borderwidth=0, command=stop_song)
+stop_btn.grid(row=0, column=4, padx=10)
+
+# Status bar
 
 status_bar = Label(root, text="", bd=1, relief=GROOVE, anchor=E)
 status_bar.pack(fill=X, side=BOTTOM, ipady=2)
